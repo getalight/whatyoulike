3ac90a3650415153e4a5c24a7d72f6f7f6a247bd
getalight <51223647+getalight@users.noreply.github.com>
2019-06-07 13:39:12 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,3 +1,3 @@
 print("you have a like")
 print("I like you very much,and I can't hold this.")
-
+print("Do you have any thoughts that you can't go home with me,and I want to give you better life,which means we can have anything we want")
